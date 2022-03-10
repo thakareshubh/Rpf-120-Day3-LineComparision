9a1c11fe272a41536653531da8d5bd3f47f999c9
thakareshubh <shubhamthakare032@gmail.com>
2022-03-10 15:21:29 +0530

    Use code for to find lenght between two point

---
+++ Uc-1-CalculateLeghthOfLine/Uc-1-CalculateLeghthOfLine/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Uc_1_CalculateLeghthOfLine
+{
+    internal class Program
+    {
+        //variables
+        double length_Of_Line;
+
+        //method for calculation
+        public void Calc(double x1, double y1, double x2, double y2)
+        {
+            double X = Math.Pow(x2 - x1, 2.0);
+            double Y = Math.Pow(y2 - y1, 2.0);
+            length_Of_Line = Math.Sqrt(X + Y);
+        }
+        public static void Main(String[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("enter x1,y1,x2,y2 values\n");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double d = Convert.ToDouble(Console.ReadLine());
+            p.Calc(a, b, c, d);
+            Console.WriteLine("Line Length is = " + p.length_Of_Line);
+        }
+
+    }
+
+}
